aa9b113972ebabcd607354b5035d2815fe2ba98a
Khushbu Patel <patelkhushbu0035@gmail.com>
2019-09-04 19:58:23 +0530

    Initialize Repo

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+    requireAuthentication: function (req, res, next) {
+       console.log('Private route hit!');
+       next();
+    },
+    logger: function (req, res, next) {
+        console.log('Request:' + req.method + ' ' + req.originalUrl);
+        next();
+    }
+};
+
+app.use(middleware.logger);
+
+// app.get('/', function (req, res) {
+//     res.send('Hello Express!');
+// });
+// app.use(middleware.requireAuthentication);
+
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+    res.send('About Us');
+});
+// app.use(middleware.requireAuthentication);
+
+// console.log(__dirname); //path to the public folder
+//app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function () {
+    console.log('Express server started on port ' + PORT + '!');
+});
